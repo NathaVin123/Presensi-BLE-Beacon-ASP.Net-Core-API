1a8021c87b003384dc70fcf3dcf74289dee5d14f
Nathaniel Vincent Wibowo <170709127@students.uajy.ac.id>
2021-07-30 14:36:54 +0700

    Minor Update

--- Presensi BLE Beacon UAJY.API/DAO/RuangBeaconDAO.cs
+++ Presensi BLE Beacon UAJY.API/DAO/RuangBeaconDAO.cs
@@ -24,7 +24,7 @@ namespace Presensi_BLE_Beacon_UAJY.API.DAO
 		                            b.PROXIMITY_UUID,
 		                            b.NAMA_DEVICE
                               FROM [SIATMAX_121212].[dbo].[tbl_kelas] kls
-	                            JOIN mst_ruang r ON kls.RUANG1 = r.RUANG
+	                            JOIN mst_ruang_2 r ON kls.RUANG1 = r.RUANG
 	                            JOIN REF_HARI h ON kls.ID_HARI1 = h.ID_HARI
 	                            JOIN REF_SESI s ON kls.ID_SESI_KULIAH1 = s.ID_SESI
 	                            JOIN mst_dosen d ON kls.NPP_DOSEN1 = d.NPP
